9d63a42acebe74d18f0b689cb7c0c1c6d0462fa0
Hans Hofner <hhofner@gmail.com>
2025-06-05 09:36:27 +0900

    Update PWA icons to PNG and add OG image

--- vite.config.ts
+++ vite.config.ts
@@ -32,6 +32,22 @@ export default defineConfig({
         start_url: '/',
         icons: [
           {
+            src: 'pwa-icon-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-icon-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-icon-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          },
+          {
             src: 'favicon.svg',
             sizes: 'any',
             type: 'image/svg+xml'
@@ -45,12 +61,6 @@ export default defineConfig({
             src: 'pwa-icon-512.svg',
             sizes: '512x512',
             type: 'image/svg+xml'
-          },
-          {
-            src: 'pwa-icon-512.svg',
-            sizes: '512x512',
-            type: 'image/svg+xml',
-            purpose: 'any maskable'
           }
         ]
       },
